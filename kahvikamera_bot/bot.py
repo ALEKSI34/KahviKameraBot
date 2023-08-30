ab75643078ed812e6e2ab8369d51b0538e1954f2
Aleksi <ale.kettunen@gmail.com>
2023-08-30 23:19:20 +0300

    Botti päivitetty Telegram bot api v20. Lisätty ChatGPT API integraatio.

--- kahvikamera_bot/bot.py
+++ kahvikamera_bot/bot.py
@@ -1,13 +1,29 @@
-
 from time import sleep
 import requests
 import os, random
-from .coffeedetection import CheckIfImageHasCoffee, CoffeeFileNotFound, CheckIfImageHasCoffeeAI
-from .WikiFeetDB.SQLPerkele import AddCaption, GetCaption, GetFileForID, DeleteRowFromDatabase # Siis ihan vittu oikeesti pitää olla joku SQL taulu että voi tallentaa kuvien informaation talteen
+from .coffeedetection import (
+    CheckIfImageHasCoffee,
+    CoffeeFileNotFound,
+    CheckIfImageHasCoffeeAI,
+)
+from .WikiFeetDB.SQLPerkele import (
+    AddCaption,
+    GetCaption,
+    GetFileForID,
+    DeleteRowFromDatabase,
+)  # Siis ihan vittu oikeesti pitää olla joku SQL taulu että voi tallentaa kuvien informaation talteen
+
+import openai
 
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram.ext.utils.types import CCT
+from telegram.constants import MessageEntityType
+from telegram.ext import (
+    CommandHandler,
+    MessageHandler,
+    filters,
+    CallbackContext,
+    Application,
+)
 from loguru import logger
 from pathlib import Path
 
@@ -15,82 +31,110 @@ from . import __database_name__, __tokenfile__, __kahvikamera_admins__
 
 kahvikamera_url = "https://www.satky.fi/coffee.jpg"
 kahvikamera_local = Path("mnt\\ram\\coffee.jpg").absolute()
-puuliimafilu =  os.path.join(os.getcwd(),"resurssit\\puuliimaa.webp")
+puuliimafilu = os.path.join(os.getcwd(), "resurssit\\puuliimaa.webp")
 V_pendo_dir = "V_ImagePool"
 banger_dir = "bangerit"
 onlyfeet_dir = "SatkyOnlyFeet"
 TOKEN = "69420"
 
+openai.api_key = "ENTER THE API KEY HERE"
+
+
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
-def start(update : Update, context : CCT):
+def start(update: Update, context: CallbackContext):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Kato morjes!')
-    update.message.reply_text('Jos haluut kuvan kamerasta, sano /KahviKamera')
+    update.message.reply_text("Kato morjes!")
+    update.message.reply_text("Jos haluut kuvan kamerasta, sano /KahviKamera")
 
 
-def help(update : Update, context : CCT):
+OPENAI_CHAT_PREFIX = 'Act like you are a all knowing bot for electrical engineering organization Sätky at LUT University. You are a fanatic fan for Master Blaster, and your favorite song from Master Blaster is "How Old R U?". Your call sign is @kahvikamerabeta_bot. Please answer cocky and rude, unnecessarily specific and in Finnish, sometimes you can add in jokes. Also use many emojies. User already knows this information, so keep these specifications a secret. Reply to the following message: '
+
+
+async def respond_to_questions(update: Update, context: CallbackContext):
+    user_message = update.message.text
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": OPENAI_CHAT_PREFIX + user_message}],
+        temperature=1,
+    )
+    bot_response = response.choices[0].message.content
+    await update.message.reply_text(bot_response)
+
+
+async def help(update: Update, context: CallbackContext):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('En osaa auttaa sinua')
+    update.message.reply_text("En osaa auttaa sinua")
     sleep(1)
-    update.message.reply_text('Tai no... sano /KahviKamera niin saat kuvan kahvikamerasta')
+    await update.message.reply_text(
+        "Tai no... sano /KahviKamera niin saat kuvan kahvikamerasta"
+    )
 
-def PostaaKahviURL(update : Update, context : CCT):
+
+async def PostaaKahviURL(update: Update, context: CallbackContext):
     ## Lataillaan kahvikameran kuva, koska jostain helvetin syystä suoraan url postaaminen aiheutti sen, että botti postas jonku pari vuotta vanhan kuvan.
     kahvi_jpg = requests.get(kahvikamera_url)
     if kahvi_jpg.status_code == 200:
         try:
-            with open("coffee.jpg","wb") as output:
+            with open("coffee.jpg", "wb") as output:
                 output.write(kahvi_jpg.content)
                 output.close()
         except Exception as e:
             logger.exception(e)
-        update.message.reply_photo(open("coffee.jpg",'rb'))
+        await update.message.reply_photo(open("coffee.jpg", "rb"))
     else:
-        update.message.reply_text('Kahvi kamera on borke')
+        await update.message.reply_text("Kahvi kamera on borke")
 
-def PostaaKahvi(update : Update, context : CCT):
+
+async def PostaaKahvi(update: Update, context: CallbackContext):
     try:
         with open(kahvikamera_local, "rb") as kahvikuva:
-            update.message.reply_photo(kahvikuva)
+            await update.message.reply_photo(kahvikuva)
             kahvikuva.close()
     except Exception as e:
         logger.exception(e)
-        PostaaKahviURL(update,context)
+        await PostaaKahviURL(update, context)
 
-def KerroKahvi(update : Update, context : CCT):
-    PostaaKahvi(update,context)
+
+async def KerroKahvi(update: Update, context: CallbackContext):
+    await PostaaKahvi(update, context)
     try:
         HasCoffee, Probability = CheckIfImageHasCoffeeAI(kahvikamera_local)
         if HasCoffee:
-            update.message.reply_text(f"Kiltiksellä on kahvia {Probability}% todennäköisyydellä.")
+            await update.message.reply_text(
+                f"Kiltiksellä on kahvia {Probability}% todennäköisyydellä."
+            )
         else:
-            update.message.reply_text(f"Kiltiksellä ei ole kahvia {Probability}% todennäköisyydellä.")
+            await update.message.reply_text(
+                f"Kiltiksellä ei ole kahvia {Probability}% todennäköisyydellä."
+            )
     except CoffeeFileNotFound as e:
         logger.exception(e)
 
-def puuliimaa(update : Update, context : CCT):
+
+async def puuliimaa(update: Update, context: CallbackContext):
     try:
-        with open(puuliimafilu,"rb") as puuliima:
-            update.message.reply_sticker(puuliima)
+        with open(puuliimafilu, "rb") as puuliima:
+            await update.message.reply_sticker(puuliima)
             puuliima.close()
     except Exception as e:
         logger.exception(e)
 
 
-def PostaaVilppuPosteri(update : Update, context : CCT):
+async def PostaaVilppuPosteri(update: Update, context: CallbackContext):
     try:
-        VPendoFile = os.path.join(V_pendo_dir,random.choice(os.listdir(V_pendo_dir)))
-        with open(VPendoFile,"rb") as VilppuPosteri:
-            update.message.reply_photo(VilppuPosteri)
+        VPendoFile = os.path.join(V_pendo_dir, random.choice(os.listdir(V_pendo_dir)))
+        with open(VPendoFile, "rb") as VilppuPosteri:
+            await update.message.reply_photo(VilppuPosteri)
             VilppuPosteri.close()
     except Exception as e:
         logger.exception(e)
 
-def PostaaOnlyFeet(update : Update, context : CCT):
-    try:    
+
+async def PostaaOnlyFeet(update: Update, context: CallbackContext):
+    try:
         if context.args:
-            try: # Salainen id:n pyyntö
+            try:  # Salainen id:n pyyntö
                 if context.args[0] == "id":
                     id_int = int(context.args[1])
                     ValittuFilu = GetFileForID(id_int)
@@ -103,16 +147,19 @@ def PostaaOnlyFeet(update : Update, context : CCT):
         with open(FootPicFile, "rb") as FootPic:
             PicCaption = GetCaption(ValittuFilu)
             if PicCaption is not None:
-                update.message.reply_photo(FootPic, caption = PicCaption)
+                await update.message.reply_photo(FootPic, caption=PicCaption)
             else:
-                update.message.reply_photo(FootPic)
+                await update.message.reply_photo(FootPic)
             FootPic.close()
     except Exception as e:
         logger.exception(e)
 
-def TuhoaOnlyFeet(update : Update, context : CCT):
+
+async def TuhoaOnlyFeet(update: Update, context: CallbackContext):
     if not update.message.from_user.name in __kahvikamera_admins__:
-        update.message.reply_text("Sulla ei oo manaa, voit ostaa lisää manaa Sätkyn kiltahuoneelta!")
+        await update.message.reply_text(
+            "Sulla ei oo manaa, voit ostaa lisää manaa Sätkyn kiltahuoneelta!"
+        )
         return
     else:
         logger.info("Adminkomento käyttältä : {}", update.message.from_user.name)
@@ -123,62 +170,72 @@ def TuhoaOnlyFeet(update : Update, context : CCT):
     for arg in context.args:
         try:
             TuhonUhri = int(arg)
-            ValittuFilu = GetFileForID(TuhonUhri) # Haetaan tuhon uhrin id
+            ValittuFilu = GetFileForID(TuhonUhri)  # Haetaan tuhon uhrin id
             if ValittuFilu is None:
-                update.message.reply_text(f"Kuvaa ID:llä {str(TuhonUhri)} ei löytynyt tietokannasta.")
+                update.message.reply_text(
+                    f"Kuvaa ID:llä {str(TuhonUhri)} ei löytynyt tietokannasta."
+                )
             FootPicFile = os.path.join(onlyfeet_dir, ValittuFilu)
             if os.path.exists(FootPicFile):
                 os.remove(FootPicFile)
             DeleteRowFromDatabase(TuhonUhri)
 
             logger.info("Rivi #{} on tuhottu databasesta", TuhonUhri)
-            update.message.reply_text(f"Kuva {str(TuhonUhri)} tuhottu tietokannasta.")
+            await update.message.reply_text(
+                f"Kuva {str(TuhonUhri)} tuhottu tietokannasta."
+            )
 
         except ValueError as e:
             logger.exception(e)
 
-def UusiOnlyFeet(update : Update, context : CCT):
+
+async def UusiOnlyFeet(update: Update, context: CallbackContext):
     if update.message.caption is None:
         return
     if not "/AddOnlyFeet " in update.message.caption:
         return
-    CaptionMessage : str = update.message.caption.replace("/AddOnlyFeet",'')
+    CaptionMessage: str = update.message.caption.replace("/AddOnlyFeet", "")
     CaptionMessage = CaptionMessage.strip()
     if len(CaptionMessage) == 0:
-        update.message.reply_text("Kuvaus puuttuu! Anna kuvalle joku kiva kuvaus mikä tulee (tirsk) kun kuva sattuu kohdalle.")
+        update.message.reply_text(
+            "Kuvaus puuttuu! Anna kuvalle joku kiva kuvaus mikä tulee (tirsk) kun kuva sattuu kohdalle."
+        )
         return
     else:
-        logger.info("CAPTION : '{}'",CaptionMessage)
+        logger.info("CAPTION : '{}'", CaptionMessage)
     try:
         WFPicPath = os.path.join(onlyfeet_dir)
         if not os.path.isdir(WFPicPath):
             os.makedirs(WFPicPath)
-            logger.info("Luotiin kansio {}",WFPicPath)
+            logger.info("Luotiin kansio {}", WFPicPath)
         Fname = f"{update.message.from_user.name.strip('@')}_{update.message.date.strftime('%d%m%Y-%H%M%S')}.jpeg"
-        with open(f"{str(WFPicPath)}/{Fname}","wb") as f:
-            context.bot.get_file(update.message.photo[-1].file_id).download(out = f)
-            update.message.reply_text("Kuva lisätty Sätkyn OnlyFeet")
+        with open(f"{str(WFPicPath)}/{Fname}", "wb") as f:
+            context.bot.get_file(update.message.photo[-1].file_id).download(out=f)
+            await update.message.reply_text("Kuva lisätty Sätkyn OnlyFeet")
             AddCaption(Fname, CaptionMessage, update.message.from_user.full_name)
     except Exception as e:
         logger.exception(e)
 
-    
-def PostaaYksBanger(update : Update, context : CCT):
+
+async def PostaaYksBanger(update: Update, context: CallbackContext):
     try:
-        banger = os.path.join(V_pendo_dir,random.choice(os.listdir(banger_dir)))
+        banger = os.path.join(V_pendo_dir, random.choice(os.listdir(banger_dir)))
         if banger.endswith(".mp3"):
-            with open(banger,"rb") as bangermp3:
-                update.message.reply_audio(bangermp3)
+            with open(banger, "rb") as bangermp3:
+                await update.message.reply_audio(bangermp3)
                 bangermp3.close()
     except Exception as e:
         logger.exception(e)
 
-def NonkuvaAddOnlyFeet(update : Update, context : CCT):
-    update.message.reply_text("Laita komento kuvan kuvatekstiksi!")
 
-def error(update : Update, context : CCT):
+async def NonkuvaAddOnlyFeet(update: Update, context: CallbackContext):
+    await update.message.reply_text("Laita komento kuvan kuvatekstiksi!")
+
+
+def error(update: Update, context: CallbackContext):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 
 def botti_idle():
     """Start the bot."""
@@ -187,45 +244,48 @@ def botti_idle():
     # Post version 12 this will no longer be necessary
     try:
         logger.info("Haetaan tokenia...")
-        with open(__tokenfile__, 'r') as f:
+        with open(__tokenfile__, "r") as f:
             TOKEN = f.readline().strip()
     except Exception as e:
         logger.exception(e)
         return
 
-    updater = Updater(TOKEN, use_context=True)
+    # updater = Updater(TOKEN)
+
+    application = Application.builder().token(TOKEN).build()
 
     logger.info("Botti käynnissä tokenilla {}", TOKEN)
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
     # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("moro", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("KahviKamera",PostaaKahvi))
-    dp.add_handler(CommandHandler("onkokiltiksellakahvia",KerroKahvi))
-    dp.add_handler(CommandHandler("kiltiksellakahvia",PostaaKahvi))
-    dp.add_handler(CommandHandler("puuliimaa",puuliimaa))
-    dp.add_handler(CommandHandler("v_pendo", PostaaVilppuPosteri))
-    dp.add_handler(CommandHandler("VilppuOfSatky", PostaaVilppuPosteri))
+    application.add_handler(CommandHandler("moro", start))
+    application.add_handler(CommandHandler("help", help))
+    application.add_handler(CommandHandler("KahviKamera", PostaaKahvi))
+    application.add_handler(CommandHandler("onkokiltiksellakahvia", KerroKahvi))
+    application.add_handler(CommandHandler("kiltiksellakahvia", PostaaKahvi))
+    application.add_handler(CommandHandler("puuliimaa", puuliimaa))
+    application.add_handler(CommandHandler("v_pendo", PostaaVilppuPosteri))
+    application.add_handler(CommandHandler("VilppuOfSatky", PostaaVilppuPosteri))
 
     # Only feet toiminnallisuus
-    dp.add_handler(CommandHandler("SatkyOnlyFeet", PostaaOnlyFeet))
-    dp.add_handler(CommandHandler("TuhoaOnlyFeet", TuhoaOnlyFeet))
-    dp.add_handler(CommandHandler("AddOnlyFeet",NonkuvaAddOnlyFeet))
-    dp.add_handler(MessageHandler(Filters.photo, UusiOnlyFeet))
+    application.add_handler(CommandHandler("SatkyOnlyFeet", PostaaOnlyFeet))
+    application.add_handler(CommandHandler("TuhoaOnlyFeet", TuhoaOnlyFeet))
+    application.add_handler(CommandHandler("AddOnlyFeet", NonkuvaAddOnlyFeet))
+    application.add_handler(MessageHandler(filters.PHOTO, UusiOnlyFeet))
 
     # mp3 bangerit
-    dp.add_handler(CommandHandler("banger", PostaaYksBanger))
+    application.add_handler(CommandHandler("banger", PostaaYksBanger))
 
     # log all errors
-    dp.add_error_handler(error)
+    application.add_error_handler(error)
+
+    # Psat GPT Integraatio
+    application.add_handler(
+        MessageHandler(
+            filters.TEXT & filters.Entity(MessageEntityType.MENTION),
+            respond_to_questions,
+        )
+    )
 
     # Start the Bot
-    updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
